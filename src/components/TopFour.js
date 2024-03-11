716e9cff219c5ed600c147bda402e317b5b949f2
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-11 16:02:53 -0700

    added song name and artist when hover for top 4

--- src/components/TopFour.js
+++ src/components/TopFour.js
@@ -27,6 +27,11 @@ const TopFour = (props) => {
                 <div key={index} className='relative w-1/5 border-4 group rounded-xl track border-dark-purple hover:border-orange-400'>
                     <img className='' src={song.albumCoverArt === 'None' ? sadMascots[index] : song.albumCoverArt} alt='track cover art' style={{borderRadius: '8px'}}></img>
                     
+                    <div className='absolute inset-0 flex-col items-center justify-center hidden p-1 rounded-lg opacity-75 group-hover:flex bg-h-grey stroke-hov-blue'></div>
+                    <div className='absolute inset-0 flex-col items-center justify-center hidden text-center align-middle group-hover:flex text-clip text-balance'>
+                        <span className='abrilfatface'>{song.name}</span>
+                        <span>{song.artist}</span>
+                    </div>
                     {loggedInUser && loggedInUser.username === thisUser.username && <svg onClick={() => openSearch(index)} className='absolute top-0 right-0 items-center justify-center hidden p-1 rounded-lg cursor-pointer group-hover:flex bg-c-grey stroke-hov-blue'xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#C6AEF2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="bevel">
                         <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
                         <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
